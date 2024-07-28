8bb28db15069f47b42f77d8a3e146a5fbc084e6a
pain-arch <himulol123@gmail.com>
2024-07-29 01:58:16 +0600

    chore: Import OpenAI and Supabase dependencies

--- src/services/file.ts
+++ src/services/file.ts
@@ -1,4 +1,18 @@
-
+import { embeddings, llm } from "@/libs/openAI";
+import { supabaseClient } from "@/libs/supabaseClient";
+import { SupabaseVectorStore } from "@langchain/community/vectorstores/supabase";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+import {
+  ChatPromptTemplate,
+  HumanMessagePromptTemplate,
+  SystemMessagePromptTemplate,
+} from "@langchain/core/prompts";
+import {
+  RunnablePassthrough,
+  RunnableSequence,
+} from "@langchain/core/runnables";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { formatDocumentsAsString } from "langchain/util/document";
 
 export interface IFile {
   id?: number | undefined;
